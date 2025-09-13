976ac54a6278cdecd42571c25b1588a37b62878b
RALPH22222 <ralphmonzales665@gmail.com>
2025-09-14 00:46:39 +0800

    First Commit 09-14-2025)=

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl bg-red-700 font-bold underline">
+      Unitorium-test for tailwind
+    </h1>
+    </>
+  )
+}
+
+export default App
