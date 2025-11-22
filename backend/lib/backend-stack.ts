28d2f20a9e141c8170d232cd4f2a3b89e9c0f5a3
Andre Lee Cuyugan <andrelee.cuyugan@gmail.com>
2025-11-22 21:02:36 +0800

    feat(backend): added auth.ts and api-gateway

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,13 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib/core";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import { HttpMethod, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import path from "path";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
 
-export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class BackendStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const auth_lambda = new NodejsFunction(this, "pms-login", {
+      functionName: "pms-login",
+      memorySize: 128,
+      runtime: Runtime.NODEJS_22_X,
+      timeout: Duration.seconds(10),
+      entry: path.resolve("src", "handlers", "auth.ts"),
+      environment: {
+        SUPABASE_KEY: StringParameter.valueForStringParameter(scope, "/pms/backend/SUPABASE_KEY"),
+      },
+    });
 
-
+    const api = new RestApi(this, "pms-api-gateway", {
+      restApiName: "pms-api-gateway",
+      deployOptions: {
+        stageName: "api",
+      },
+    });
+    const auth = api.root.addResource("auth");
+    auth.addMethod(HttpMethod.POST, new LambdaIntegration(auth_lambda));
   }
 }
