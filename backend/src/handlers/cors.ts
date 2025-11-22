04e59a7b88624ca1cd704159c01ff998bcc7f604
vodh1 <andrelee.cuyugan@gmail.com>
2025-11-22 23:33:39 +0800

    feat(backend): add cors lambda and resource path

---
+++ backend/src/handlers/cors.ts
@@ -0,0 +1,13 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS'
+    },
+    body: JSON.stringify({})
+  };
+};
