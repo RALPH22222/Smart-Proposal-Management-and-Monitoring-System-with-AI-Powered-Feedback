740303a50dbb0ba553c66d6a92eccdf9fec7489f
vodh1 <andrelee.cuyugan@gmail.com>
2025-11-23 15:39:28 +0800

    feat(backend): allow credentials on cors

--- backend/src/handlers/cors.ts
+++ backend/src/handlers/cors.ts
@@ -4,6 +4,7 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   return {
     statusCode: 200,
     headers: {
+      'Access-Control-Allow-Credentials': true,
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS'
