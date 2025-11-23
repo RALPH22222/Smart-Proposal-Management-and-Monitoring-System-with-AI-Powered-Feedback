49ff4a57b330fda59b65a5eb31e51a5c26eb3c13
vodh1 <andrelee.cuyugan@gmail.com>
2025-11-23 15:48:56 +0800

    feat(backend): update allowed origins

--- backend/src/handlers/cors.ts
+++ backend/src/handlers/cors.ts
@@ -1,11 +1,20 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
+const allowedOrigins = [
+  "http://localhost:5173"
+];
+
 export const handler: APIGatewayProxyHandler = async (event) => {
+  const origin =
+     event.headers?.origin ||
+     event.headers?.Origin || // some browsers send 'Origin'
+     "";
+
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': allowedOrigins.includes(origin) ? origin : "",
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Methods': 'POST, OPTIONS'
     },
