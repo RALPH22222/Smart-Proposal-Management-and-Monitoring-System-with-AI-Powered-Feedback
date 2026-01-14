74207745d6a09f0befa82742e785498fbd78293a
RALPH22222 <ralphmonzales665@gmail.com>
2026-01-14 18:00:18 +0800

    up1

--- SPMAMS/constants/Config.ts
+++ SPMAMS/constants/Config.ts
@@ -1,10 +1,11 @@
 import { Platform } from 'react-native';
 
 const LOCAL_IP = '10.166.68.253'; 
+const PORT = '3000';
 
-const PORT = '8000'; 
 export const API_URL = Platform.select({
-    android: `http://${LOCAL_IP}:${PORT}`,
-    ios: `http://${LOCAL_IP}:${PORT}`,
-    default: `http://localhost:${PORT}`,
+    android: `http://${LOCAL_IP}:${PORT}/api`,
+    ios: `http://${LOCAL_IP}:${PORT}/api`,
+    
+    default: `http://localhost:${PORT}/api`,
 });
